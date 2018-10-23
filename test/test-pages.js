13501a032ef7d07308d8e58947f1a44000082ab7
dipi46 <chapa96_12@hotmail.com>
2018-10-23 19:30:03 -0300

    added new test

---
+++ test/test-pages.js
@@ -0,0 +1,22 @@
+var expect  = require('chai').expect;
+var request = require('request');
+
+var server = require('../bin/www');
+
+it('helloworld page example', function(done) {
+    request('http://localhost:3000/helloworld' , function(error, response, body) {
+        expect(body).to.equal("<!DOCTYPE html><html><body>'Hello, World!'</body></html>");
+        done();
+    });
+});
+
+describe('pages', function(){
+      it('should respond to GET',function(done){
+        request.get('http://localhost:3000', function(error, response, body) {
+          expect(response.statusCode).to.equal(200);
+          done();
+        });
+      });
+
+
+    });
